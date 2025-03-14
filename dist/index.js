453471c98a274dc3c4f47de7b5a1b4055f1d82cb
robertms55 <60856842+robertms55@users.noreply.github.com>
2025-03-14 20:25:36 -0300

    Delete dist directory

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ContaCorrente_1 = require("./models/ContaCorrente");
-const ContaPoupanca_1 = require("./models/ContaPoupanca");
-const conta1 = new ContaCorrente_1.ContaCorrente("Roberto Moreira", 1000, 500);
-conta1.depositar(500);
-conta1.sacar(1800); // Testando cheque especial
-const conta2 = new ContaPoupanca_1.ContaPoupanca("Ana Souza", 2000, 0.5);
-conta2.aplicarRendimento();
